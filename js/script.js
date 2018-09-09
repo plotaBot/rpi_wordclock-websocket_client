197d4052658b0d07cf0711b23f7ec7c5825d6071
plotaBot <plotaBot@users.noreply.github.com>
2018-09-09 21:59:04 +0200

    initial upload 2
    wrong folder <_>

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-(function($){
 $(function(){
 	  
 	// Settings
 	var host = window.location.hostname;
-	//host = "esp8266_02.local";
+	//host = "hostIP";
 
-	var ws_url = 'ws://' + host + ':81';
+	var ws_url = 'ws://' + host + ':8081';
 	var connection;
 	var ws_waiting = 0;
 
@@ -32,7 +31,7 @@ $(function(){
 	// ******************************************************************
 	function init() {
 		console.log("Connection websockets to:", ws_url);
-		connection = new WebSocket(ws_url, ['arduino']);
+		connection = new WebSocket(ws_url, ['wordclock']);
 		
 				
 		// When the connection is open, send some data to the server
@@ -59,27 +58,18 @@ $(function(){
 	
 	// ******************************************************************
 	// Modes
-	// ******************************************************************		
-	$("#pane1").on("click", ".btn_mode_static", function() {
-		if($(this).attr("multi") == "1"){
-			var outArray = "?";
-			for(var i=0; i<=8; i++){
-				if(document.getElementById('ignite'+i).checked){
-					outArray += '1';
-				}
-				else {outArray += '0';}
-			}
-			wsSendCommand(outArray);
-		}
-		else{
-			var pin = $(this).attr("pinNr");
-			wsSendCommand("*" + pin);
-			$(".btn_mode, .btn_mode_static").removeClass("red").addClass("blue");
-			btn.removeClass("blue").addClass("red");
-		}
-	});	
+	// ******************************************************************
+	$("#leftBtn").on("click", function() {
+			wsSendCommand('left');
+	});
 	
+	$("#returnBtn").on("click", function() {
+			wsSendCommand('return');
+	});
 	
+	$("#rightBtn").on("click", function() {
+			wsSendCommand('right');
+	});
 	
 	// ******************************************************************
 	// WebSocket commands
@@ -94,21 +84,7 @@ $(function(){
 		}
 	}	
 	
-	// ******************************************************************
-	// main
-	// ******************************************************************
-	var modes_html = "";
-		for (var i=0; i<=8; i++) {
-			modes_html += '<div class="col s12 m6 l6 btn_grid"><div class="left switch"><label>';
-			modes_html += '<input type="checkbox" id="ignite' + i + '"><span class="lever"></span></label></div>';
-			modes_html += '<a class="btn waves-effect waves-light btn_mode_static blue" name="action" multi="0" pinNr="' + i + '">Ignite N°'+i;
-			modes_html += '</a></div>';
-		}
-			
-	document.getElementById("ignite").innerHTML = modes_html;
-	
 	init();
 	
 	
 }); // end of document ready
-})(jQuery); // end of jQuery name space
